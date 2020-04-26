2a850fe1317831b4f026681849de9ac9e7e79928
Andy Carlberg <andycarlberg@gmail.com>
2020-04-26 16:39:54 -0500

    Add eslint/prettier.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-export default function Index() {
-    return (
-      <div>
-        <p>Hello Next.js</p>
-      </div>
-    );
-  }
-  
+import React from 'react';
+
+export default function Index(): React.FC {
+  return (
+    <div>
+      <p>Hello Next.js</p>
+    </div>
+  );
+}
