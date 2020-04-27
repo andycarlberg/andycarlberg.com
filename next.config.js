b4a91892bc126a20b520721692f6c3ebcc98084a
Andy Carlberg <andy.carlberg@bounteous.com>
2020-04-27 10:27:40 -0500

    Add Markdown support.

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+});
+
+module.exports = withMDX({
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+});
--- pages/index.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ReactElement } from 'react';
-
-export default function Index(): ReactElement {
-  return <p>Hello Next.js</p>;
-}
