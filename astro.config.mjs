cf245d02dfaf2a08f98a733c543f945f617b29b1
Andy Carlberg <andycarlberg@gmail.com>
2025-11-14 15:00:32 -0600

    Initial commit.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        lucide: ["sun", "moon"],
+        "simple-icons": ["github", "linkedin", "x"],
+      },
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
