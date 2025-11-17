263f06024e9280c3c55758f1b9ce49d618a63281
Andy Carlberg <andycarlberg@gmail.com>
2025-11-17 16:28:46 -0600

    Update site metadata configutation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,32 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import { loadEnv } from "vite";
 
-// https://astro.build/config
+const env = loadEnv(process.env.NODE_ENV ?? "development", process.cwd(), "");
+
+const VERCEL_ENV = env.VERCEL_ENV;
+let siteUrl = "http://localhost:4321"; // Default for local development
+
+if (VERCEL_ENV === "production") {
+  // Production environment
+  siteUrl = env.PUBLIC_VERCEL_URL
+    ? `https://${env.PUBLIC_VERCEL_URL}`
+    : "https://www.andycarlberg.com";
+} else if (VERCEL_ENV === "preview") {
+  // Vercel Preview environment
+  siteUrl = env.VERCEL_URL ? `https://${env.VERCEL_URL}` : siteUrl;
+}
+
 export default defineConfig({
-  site: "https://example.com",
+  site: siteUrl,
   integrations: [
     mdx(),
     sitemap(),
     icon({
       include: {
         lucide: ["sun", "moon"],
-        "simple-icons": ["github", "linkedin", "x"],
+        "simple-icons": ["github", "linkedin"],
       },
     }),
   ],
