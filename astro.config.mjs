c13f5422e1f7b262956c131d10a2dce0fbfc75ec
Andy Carlberg <andycarlberg@gmail.com>
2025-11-18 14:10:30 -0600

    fix(deps,blog): fix deployment errors found on Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import { loadEnv } from "vite";
+import vercel from "@astrojs/vercel";
 
 const env = loadEnv(process.env.NODE_ENV ?? "development", process.cwd(), "");
 
@@ -13,28 +14,33 @@ const VERCEL_ENV = env.VERCEL_ENV;
 let siteUrl = "http://localhost:4321"; // Default for local development
 
 if (VERCEL_ENV === "production") {
-	// Production environment
-	siteUrl = env.PUBLIC_VERCEL_URL
-		? `https://${env.PUBLIC_VERCEL_URL}`
-		: "https://www.andycarlberg.com";
+  // Production environment
+  siteUrl = env.PUBLIC_VERCEL_URL
+    ? `https://${env.PUBLIC_VERCEL_URL}`
+    : "https://www.andycarlberg.com";
 } else if (VERCEL_ENV === "preview") {
-	// Vercel Preview environment
-	siteUrl = env.VERCEL_URL ? `https://${env.VERCEL_URL}` : siteUrl;
+  // Vercel Preview environment
+  siteUrl = env.VERCEL_URL ? `https://${env.VERCEL_URL}` : siteUrl;
 }
 
 export default defineConfig({
-	site: siteUrl,
-	integrations: [
-		mdx(),
-		sitemap(),
-		icon({
-			include: {
-				lucide: ["sun", "moon"],
-				"simple-icons": ["github", "linkedin"],
-			},
-		}),
-	],
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  site: siteUrl,
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      include: {
+        lucide: ["sun", "moon"],
+        "simple-icons": ["github", "linkedin"],
+      },
+    }),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: "server",
+  adapter: vercel({
+    edge: false,
+    analytics: true,
+  }),
 });
