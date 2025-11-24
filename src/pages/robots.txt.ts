eb436eb6f4d44490c5c93fd0b8086b982f5da3ae
Andy Carlberg <andycarlberg@gmail.com>
2025-11-24 09:04:07 -0600

    feat(layout): add robots.txt file

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,37 @@
+import type { APIRoute } from "astro";
+
+const PREVIEW_ROBOTS_TXT = `
+User-agent: *
+Disallow: /
+`;
+
+const PRODUCTION_ROBOTS_TXT = (sitemapUrl: string) => `
+User-agent: *
+Allow: /
+
+# Exclude common resource directories from being explicitly crawled, 
+# as search engines usually handle these implicitly.
+Disallow: /assets/
+Disallow: /images/
+Disallow: /styles/
+
+# Disallow the contact form API route
+Disallow: /api/contact/
+
+Sitemap: ${sitemapUrl}
+`;
+
+export const GET: APIRoute = async ({ url }) => {
+	const isPreview = process.env.VERCEL_ENV !== "production";
+	const sitemapUrl = new URL("sitemap-index.xml", url).href;
+
+	const content = isPreview
+		? PREVIEW_ROBOTS_TXT.trim()
+		: PRODUCTION_ROBOTS_TXT(sitemapUrl).trim();
+
+	return new Response(content, {
+		headers: {
+			"Content-Type": "text/plain; charset=utf-8",
+		},
+	});
+};
